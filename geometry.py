aba8e2774df579de429525afed5ac1d1f4c17753
Kstrick01 <kstrick027@gmail.com>
2022-11-08 14:25:04 -0500

    Add files via upload

---
+++ geometry.py
@@ -0,0 +1,11 @@
+def area_of_rectangle():
+    """
+    This function returns the area of the given rectangle
+    """
+    
+    length = int(input("Please enter a number"))
+    breadth = int(input("Please enter a number"))
+    area = length * breadth
+
+    return area
+
